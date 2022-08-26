4eba61f9fab0b92133df6087be55bd74da1e8e8e
Anton Shchukin <a.a.shchukin@gmail.com>
2022-08-26 19:24:21 +0700

    animation draft

--- development/scripts/_listing.js
+++ development/scripts/_listing.js
@@ -1,13 +1,12 @@
 (function($) {
 
-    $('.listing__flip-handler').on('click', function () {
-        $('.listing__item--current').addClass('full-card--open');
-    });
-
     $('.listing__item').on('click', function () {
         $('.listing').addClass('listing--expanded');
-        $('.listing__item--current').removeClass('listing__item--current');
         $(this).addClass('listing__item--current');
+        $('.listing__item--current').addClass('full-card--open');
+        $('.listing__item:not(.listing__item--current)').fadeOut();
+        $('.deck__question').slideUp(600);
+        $("html, body").animate({scrollTop: 0}, "slow");
     });
 
 })(jQuery);
